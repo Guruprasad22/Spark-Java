02d632f4e3e93679217cae63f4125856fb3d1a38
unknown <guru.bobbi@gmail.com>
2015-03-01 21:12:11 -0500

    renamed file

--- src/main/java/com/playground/WordCount.java
+++ src/main/java/com/playground/WordCount.java
@@ -1,18 +1,20 @@
 package com.playground;
 
 import java.util.Arrays;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
+/**
+ * word count example using geeky transformation methods
+ */
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.SparkConf;
 
 import scala.Tuple2;
 
-public class SparkDriver {
+public class WordCount {
 
 	public static void main(String[] args) {
 		
